373e81ff48ec889d2eb62bd1db4c6b27b8614daa
DRidleyS <dorianridley@gmail.com>
2025-01-05 14:11:04 -0800

    torture2

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ app = Flask(__name__)
 
 # Flask-Mail configuration
 app.config['MAIL_SERVER'] = os.getenv('SMTP_SERVER')
-app.config['MAIL_PORT'] = int(os.getenv('SMTP_PORT'))
+app.config['MAIL_PORT'] = int(os.getenv('SMTP_PORT', 587))
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.getenv('EMAIL_USER')
 app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_PASS')
